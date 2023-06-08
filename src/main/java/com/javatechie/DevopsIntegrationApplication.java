5b3c327313aadbd8d71ebe45e3f57ad48a396b0c
yathish12 <103912830+yathish12@users.noreply.github.com>
2023-06-08 18:18:46 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to DevOps Hackathon";
+		return <b> "welcome to DevOps Hackathon" </b>;
 	}
 
 	public static void main(String[] args) {
